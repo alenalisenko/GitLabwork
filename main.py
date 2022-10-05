811f19ed4a3a7cb82926b1c4980f11b7197a6081
Alena Lisenko <lilifoxy.2004@gmail.com>
2022-10-05 17:43:15 +0300

    game

--- main.py
+++ main.py
@@ -38,3 +38,55 @@ food = turtle.Turtle()
 food.shape('circle')
 food.penup()
 food.goto(randrange(-300, 300, 20), randrange(-300, 300, 20))
+
+# snake control
+screen.onkeypress(lambda: snake[0].setheading(90), 'Up')
+screen.onkeypress(lambda: snake[0].setheading(270), 'Down')
+screen.onkeypress(lambda: snake[0].setheading(180), 'Left')
+screen.onkeypress(lambda: snake[0].setheading(0), 'Right')
+screen.listen()
+
+while True:
+
+    # creating a new segment of the snake
+    # and redraw a food for the snake
+    if snake[0].distance(food) < 10:
+        food.goto(randrange(-300, 300, 20), randrange(-300, 300, 20))
+        snake_segment = turtle.Turtle()
+        snake_segment.shape('square')
+        snake_segment.color('gray')
+        snake_segment.penup()
+        snake.append(snake_segment)
+
+    # snake body movement
+    for i in range(len(snake)-1, 0, -1):
+        x = snake[i-1].xcor()
+        y = snake[i-1].ycor()
+        snake[i].goto(x, y)
+
+    # snake head movement
+    snake[0].forward(20)
+
+    screen.update()
+
+    # snake collision with border
+    x_cor = snake[0].xcor()
+    y_cor = snake[0].ycor()
+    if x_cor > 300 or x_cor < -300:
+        screen.bgcolor('red')
+        break
+    if y_cor > 300 or y_cor < -300:
+        screen.bgcolor('red')
+        break
+
+    # snake collision with itself
+    for i in snake[1:]:
+        i = i.position()
+        if snake[0].distance(i) < 10:
+            BREAK_FLAG = True
+    if BREAK_FLAG:
+        screen.bgcolor('red')
+        break
+    time.sleep(0.2)
+
+screen.mainloop()
